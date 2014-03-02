388fa978a3238eb22338c9f3763da420f7a4f8fb
spamela <nunezpam@gmail.com>
2014-03-02 12:18:45 -0500

    Initialize repo, first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'db7cfcf0a7ca3d4d6bc49ecccde48c645c12ba7167fc925de0a8383f8da655c5b04e21e774ff2b2a4a88619cd77fa26435f0ac1d5ca881b8f19b1095f4979549'
